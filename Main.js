6018ffa5dc3567c3390edef2663b8096d3df4d10
evanac21 <evanac21@gmail.com>
2019-02-04 11:31:47 -0800

    Re-added all files

--- Main.js
+++ Main.js
@@ -1,15 +1,20 @@
 const si = require('systeminformation');
 const http = require('http');
 const eip = require('externalip');
+const rls = require('readline-sync');
+var parse, userIP, cpuName, hasbatt, osdist, osplat, fip, sip, uname, memtotal;
 
-var parse, userIP, cpuName, hasbatt, osdist, osplat, fip, sip;
-
+uname = rls.question("Username for this PC?: ");
 //Grabs network interface data
 si.networkInterfaces(function(data) {
   fip = data[0].ip4;
   sip = data[1].ip4;
 });
 
+si.mem(function(data) {
+  var unparseMem = data.total
+  memtotal = unparseMem / 1000000;
+});
 //CPU brand fetching using package
 si.cpu(function(data) {
    cpuName = data.brand;
@@ -34,11 +39,13 @@ eip(function(err, ip) {
 //Creating viewable web page through user's Internal IP (localhost)
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("<h1>IP: </h1>" + userIP + "<br />");
-  res.write("<h1>CPU: </h1>" + cpuName + "<br />");
-  res.write("<h1>Has Battery: </h1>" + hasbatt + "<br />");
-  res.write("<h1>OS Info: </h1>" + "Platform: " + osplat + "<br />" + "Distro: " + osdist + "<br />"); 
-  res.write("<h1>Network Interfaces </h1>" + "IPV4 (1): " + fip + "<br />" + "IPV4 (2): " + sip);
+  res.write("<h1>System Information for: "+ uname +"</h1>");
+  res.write("<h3>IP: </h3>" + userIP + "<br />");
+  res.write("<h3>CPU: </h3>" + cpuName + "<br />");
+  res.write("<h3>RAM: </h3>" + memtotal + " mb" + "<br />");
+  res.write("<h3>Has Battery: </h3>" + hasbatt + "<br />");
+  res.write("<h3>OS Info: </h3>" + "Platform: " + osplat + "<br />" + "Distro: " + osdist + "<br />"); 
+  res.write("<h3>Network Interfaces </h3>" + "IPV4 (1): " + fip + "<br />" + "IPV4 (2): " + sip);
   res.end();
 }).listen(8080);
 
